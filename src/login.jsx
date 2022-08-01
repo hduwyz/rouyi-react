35e686ae64fc422a683414f9d6ab22f13e54bedd
wangyz <wangyuzhong@cmdi.chinamobile.com>
2022-08-01 16:19:02 +0800

    代码初始化，配置antd全局引用

---
+++ src/login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class LoginMain extends Component{
+
+    render() {
+        return (
+            <div>若依后台管理系统</div>
+            
+
+        )
+    }
+}
+
+export default LoginMain
